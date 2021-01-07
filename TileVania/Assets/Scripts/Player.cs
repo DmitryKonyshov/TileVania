70405ec31d8fca1692a4a1f7db28c1826dd36a7a
Dmitry <dmitrykonyshov@gmail.com>
2021-01-07 16:18:02 +0200

    Move Player Horizontally

---
+++ TileVania/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float runSpeed = 5f;
+    private Rigidbody2D _myRigidBody;
+    
+    private void Start()
+    {
+        _myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Run();
+    }
+    
+    private void Run()
+    {
+        var controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        var playerVelocity = new Vector2(controlThrow * runSpeed, _myRigidBody.velocity.y);
+        _myRigidBody.velocity = playerVelocity;
+    }
+}
