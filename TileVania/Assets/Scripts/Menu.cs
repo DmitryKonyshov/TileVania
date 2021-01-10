caefc4bdc3dc8ebb12d7eccc6325084917e1c4cd
Dmitry <dmitrykonyshov@gmail.com>
2021-01-10 11:14:45 +0200

    Start And Success Screens

---
+++ TileVania/Assets/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
